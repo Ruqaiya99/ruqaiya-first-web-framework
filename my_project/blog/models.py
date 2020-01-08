80947c864efe3a74627a01437bee2dbee6101d77
Ruqaiya99 <59636926+Ruqaiya99@users.noreply.github.com>
2020-01-07 22:37:38 -0800

    Add files via upload

---
+++ my_project/blog/models.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import post
+
+admin.site.register(Post)
+
+
+
+python manage.py createsuperuser
+(my_env) ~/my_project python manage.py createsuperuser
+
+# Create your models here.
